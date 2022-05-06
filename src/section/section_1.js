5a0f1842523e36a83ca6d03cb265c4dcbb8ff903
poohghg <38716228+poohghg@users.noreply.github.com>
2022-05-06 16:34:45 +0900

    commit

--- src/section/section_1.js
+++ src/section/section_1.js
@@ -1,19 +1,20 @@
+const app = document.querySelector("#app");
+console.log("app", app);
+
 /*
 문제1 
   100이하의 자연수 A, B, C를 입력받아 세 수 중 가장 작은 값을 출력하는 프로그램을 작성하 세요.(정렬을 사용하면 안됩니다)
 */
-function solution(a, b, c) {
-  let minNum;
+function solution_1(a, b, c) {
+  let anser;
 
   if (a < b) {
-    minNum = a;
+    anser = a;
   } else {
-    minNum = b;
+    anser = b;
   }
 
-  if (minNum < c) {
-    return minNum;
-  }
-  return c;
+  if (c < anser) anser = c;
+  return anser;
 }
-console.log("anser : ", solution(3, 4, 5));
+console.log("anser : ", solution_1(3, 4, 5));
