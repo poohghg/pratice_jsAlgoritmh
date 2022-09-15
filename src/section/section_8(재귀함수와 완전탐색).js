073d803c798296e3631e151c86d164411898ebdb
khg <poohghg@naver.com>
2022-09-15 21:40:04 +0900

    문제풀이

--- src/section/section_8(재귀함수와 완전탐색).js
+++ src/section/section_8(재귀함수와 완전탐색).js
@@ -174,11 +174,82 @@ function solution_4(c, n, arr) {
       max = Math.max(sum, max);
       return;
     }
-    console.log(l);
     travers(l + 1, sum + arr[l]);
     travers(l + 1, sum);
   }
   travers(0, 0);
   return max;
 }
-console.log(solution_4(259, 3, [81, 58, 42]));
+// console.log(solution_4(259, 3, [81, 58, 42]));
+/**
+ * N개의 원소로 구성된 자연수 집합이 주어지면, 이 집합을 두 개의 부분집합으로 나누었을 때 두 부분집합의 원소의 합이 서로 같은 경우가 존재하면 “YES"를 출력하고, 그렇지 않으면 ”NO"를 출력하는 프로그램을 작성하세요.
+ * 둘로 나뉘는 두 부분집합은 서로소 집합이며, 두 부분집합을 합하면 입력으로 주어진 원래의 집합이 되어 합니다.
+ */
+function solution_5(n, arr) {
+  const halfSum = arr.reduce((a, b) => a + b, 0) / 2;
+  // console.log(halfSum);
+  let answer = 'NO';
+  function travers(L, sum) {
+    if (answer === 'YES') return;
+    if (L === n) {
+      if (halfSum === sum) answer = 'YES';
+      return;
+    }
+    travers(L + 1, sum + arr[L]);
+    travers(L + 1, sum);
+  }
+  travers(0, 0);
+  return answer;
+  // return travers(0, 0) ? ;
+}
+// console.log(solution_5(6, [1, 3, 5, 6, 7, 10]));
+function solution_6(n, m, arr) {
+  let max = Number.MIN_SAFE_INTEGER;
+  function travers(L, sum, time) {
+    if (time > m) return;
+    if (L === n) {
+      console.log('sum', sum, 'time', time);
+      max = Math.max(max, sum);
+      return;
+    }
+    travers(L + 1, sum + arr[L][0], time + arr[L][1]);
+    travers(L + 1, sum, time);
+  }
+  travers(0, 0, 0);
+  return max;
+}
+// console.log(
+//   solution_6(5, 20, [
+//     [10, 5],
+//     [25, 12],
+//     [15, 8],
+//     [6, 3],
+//     [7, 4],
+//   ]),
+// );
+/**
+ * 1부터 N까지 번호가 적힌 구슬이 있습니다.
+ * 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열 하는 방법을 모두 출력합니다.
+ */
+function solution_7(n, m) {
+  const answer = [];
+  let oriArr = [];
+  const tmp = Array.from({ length: n }).map((_, i) => i + 1);
+  for (let i = 0; i < m; i++) {
+    oriArr.push(tmp);
+  }
+  function travers(L, arr) {
+    if (L === m) return answer.push(arr);
+    //  1   2   3
+    // 123 123 123
+    for (let i = 0; i < oriArr[L].length; i++) {
+      travers(L + 1, arr.concat(oriArr[L][i]));
+    }
+  }
+  travers(0, []);
+  console.log(answer.join('\n'));
+  console.log(answer.length);
+}
+console.log(solution_7(3, 2));
+// const arr = [];
+// console.log(arr.push(5));
