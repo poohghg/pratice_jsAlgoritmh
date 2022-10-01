89180f74eb6bd80f14f0938e9f664a86d68bf8a4
khg <poohghg@naver.com>
2022-10-01 23:24:03 +0900

    문제풀이

--- src/section/section_9(그래프와 탐색).js
+++ src/section/section_9(그래프와 탐색).js
@@ -51,6 +51,26 @@ class Grape {
     // travers.call(Grape, start);
     return result;
   }
+  // 넓이 우선탐색
+  BFS(start) {
+    if (!this.list[start]) return;
+    const visited = {};
+    const queue = [];
+    queue.push(start);
+
+    let currVertex;
+    while (queue.length) {
+      currVertex = queue.shift();
+      if (!visited[currVertex]) {
+        visited[currVertex] = true;
+        for (const neighbor of this.list[currVertex]) {
+          queue.push(neighbor);
+        }
+      }
+      // currVertex.forEach
+    }
+    return Object.keys(visited);
+  }
 }
 
 //          A
@@ -74,8 +94,8 @@ g.addEdge('C', 'E');
 g.addEdge('D', 'E');
 g.addEdge('D', 'F');
 g.addEdge('E', 'F');
-console.log(g.DFS('A'));
-
+// console.log(g.DFS('A'));
+console.log(g.BFS('A'));
 // const t = Array.from(Array(5), () => Array(5).fill(0));
 // console.log(t);
 
@@ -158,19 +178,19 @@ function solution_2(N, M, info) {
   travers(1);
   return answer;
 }
-console.log(
-  solution_2(5, 9, [
-    [1, 2],
-    [1, 3],
-    [1, 4],
-    [2, 1],
-    [2, 3],
-    [2, 5],
-    [3, 4],
-    [4, 2],
-    [4, 5],
-  ]),
-);
+// console.log(
+//   solution_2(5, 9, [
+//     [1, 2],
+//     [1, 3],
+//     [1, 4],
+//     [2, 1],
+//     [2, 3],
+//     [2, 5],
+//     [3, 4],
+//     [4, 2],
+//     [4, 5],
+//   ]),
+// );
 
 /**
  * 7*7 격자판 미로를 탈출하는 경로의 가지수를 출력하는 프로그램을 작성하세요.
@@ -212,15 +232,126 @@ function solution_3(info, start = [1, 1], end = [7, 7]) {
   travers(startX - 1, startY - 1);
   console.log(answer);
 }
+// console.log(
+//   solution_3([
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 1, 1, 1, 1, 1, 0],
+//     [0, 0, 0, 1, 0, 0, 0],
+//     [1, 1, 0, 1, 0, 1, 1],
+//     [1, 1, 0, 0, 0, 0, 1],
+//     [1, 1, 0, 1, 1, 0, 0],
+//     [1, 0, 0, 0, 0, 0, 0],
+//   ]),
+// );
+/**
+ * 현수는 송아지를 잃어버렸다. 다행히 송아지에는 위치추적기가 달려 있다.
+ * 현수의 위치와 송아 지의 위치가 수직선상의 좌표 점으로 주어지면 현수는 현재 위치에서 송아지의 위치까지 다음 과 같은 방법으로 이동한다.
+ * 송아지는 움직이지 않고 제자리에 있다.현수는 스카이 콩콩을 타고 가는데 한 번의 점프로 앞으로 1, 뒤로 1, 앞으로 5를 이동할 수 있다.
+ * 최소 몇 번의 점프로 현수가 송아지의 위치까지 갈 수 있는지 구하는 프로그램을 작성 하세요.
+ */
+function solution_4(s, e) {
+  // let answer = 0;
+  const moves = [-1, 1, 5];
+  const distance = Array.from({ length: 10001 });
+
+  distance[s] = 0;
+  const visited = { s: true };
+  const queue = [s];
+
+  let currValue;
+  let newDistance;
+  while (queue.length) {
+    currValue = queue.shift();
+    for (const x of moves) {
+      newDistance = currValue + x;
+      if (newDistance === e) {
+        return distance[currValue] + 1;
+      }
+      if (!visited[newDistance]) {
+        visited[newDistance] = true;
+        distance[newDistance] = distance[currValue] + 1;
+        queue.push(newDistance);
+      }
+    }
+    // break;
+  }
+}
+// console.log(solution_4(5, 14));
+
+/**
+ * N*N의 섬나라 아일랜드의 지도가 격자판의 정보로 주어집니다.
+ * 각 섬은 1로 표시되어 상하좌 우와 대각선으로 연결되어 있으며, 0은 바다입니다.
+ * 섬나라 아일랜드에 몇 개의 섬이 있는지 구하는 프로그램을 작성하세요.
+ */
+// BFS
+function solution_5(n, arr) {
+  let answer = 0;
+  const dx = [-1, 0, 1, 0, -1, 1, 1, -1];
+  const dy = [0, 1, 0, -1, -1, 1, -1, 1];
+  const queue = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][j] === 1) {
+        arr[i][j] = 0;
+        queue.push([i, j]);
+        while (queue.length) {
+          let [tmpx, tmpy] = queue.shift();
+          for (let k = 0; k < dx.length; k++) {
+            let nx = tmpx + dx[k];
+            let ny = tmpy + dy[k];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            if (arr[nx][ny] === 1) {
+              arr[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
+        answer++;
+      }
+    }
+  }
+  return answer;
+}
+
+// DFS
+function solution_6(n, arr) {
+  let answer = 0;
+  const dx = [-1, 0, 1, 0, -1, 1, 1, -1];
+  const dy = [0, 1, 0, -1, -1, 1, -1, 1];
+
+  function travers(x, y) {
+    for (let i = 0; i < dx.length; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
+      if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+      if (arr[nx][ny] === 1) {
+        arr[nx][ny] = 0;
+        travers(nx, ny);
+      }
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][j] === 1) {
+        arr[i][j] = 0;
+        travers(i, j);
+        answer++;
+      }
+    }
+  }
+  return answer;
+}
 
 console.log(
-  solution_3([
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0],
-    [0, 0, 0, 1, 0, 0, 0],
-    [1, 1, 0, 1, 0, 1, 1],
-    [1, 1, 0, 0, 0, 0, 1],
+  solution_6(7, [
+    [1, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 1],
     [1, 1, 0, 1, 1, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0],
+    [1, 0, 1, 0, 1, 0, 0],
   ]),
 );
